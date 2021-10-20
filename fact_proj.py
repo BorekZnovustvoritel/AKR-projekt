d0ec10d038c27f0089f6aa19ee9dd7a45890545f
Marek Szymutko <xszymu01@vutbr.cz>
2021-10-20 10:47:40 +0200

    Přidán výpočet privátního klíče a trochu více komunikace s uživatelem.

--- fact_proj.py
+++ fact_proj.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from rsa import RSA_key
+from Crypto.Util import number
 
 def rsa_factorization(num):
     max_fact = int(sqrt(num))
@@ -16,6 +17,11 @@ if __name__ == "__main__":
     print(f"Modulo is: {key.mod}")
     print(f"Public exponent is: {key.public}")
     ref = perf_counter()
-    print(rsa_factorization(key.mod))
+    print("Cracking, please wait...")
+    p, q = rsa_factorization(key.mod)
+    print([p, q])
     print("Time in seconds: %.2f" % (perf_counter() - ref))
-    input()
+    print("Generating Private key...")
+    priv_key = number.inverse(key.public, (p - 1) * (q - 1))
+    print(f"Private key is: {priv_key}")
+    input("Program finished, press any key.")
