945fc299f8dd5813e135378d7eee6517e4c3f5e2
Philipeer <hamiltoon@seznam.cz>
2021-11-20 18:34:55 +0100

    Přidána funkce fermat(num) pro filtr čísel na prvočísla

--- prime_test.py
+++ prime_test.py
@@ -1,11 +1,4 @@
 def fermat(num):
-    a = False
-    b = False
     if ((2 ** num) - 2) % num == 0:
-        a = True
-    if (2 ** (num - 1) % num) == 1:
-        b = True
-    if a & b is True:
         return True
-    else:
-        return False
+    else: return False
