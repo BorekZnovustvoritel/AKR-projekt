07e6b2ff1119fe3d94a16f87cd176080a59e57ae
Krupass <92521540+Krupass@users.noreply.github.com>
2021-11-28 10:45:09 +0100

    Komentáře

--- functionalities/sqroot.py
+++ functionalities/sqroot.py
@@ -1,13 +1,11 @@
 def sqroot(num):
     """Natural square root (decimal points are ignored).
     Needed because math.sqrt breaks with numbers bigger than 2 to the power of 1024."""
-    lenght = len(str(num))
-    half = str(num)[0:(lenght//2)]
-    #print(half)
+    lenght = len(str(num))#convert number to string
+    half = str(num)[0:(lenght//2)]#create a substring that is roughly a half of the number
     i = (int("1" + "0" * (lenght//2)))
-    #print(i)
-    root = int(half)
-    while(True):
+    root = int(half)#convert the substring into number
+    while(True):#infinite loop that adds and substracts until the root equals the root of number (without decimals)
         if(i < 1):
             break
         if(((pow(root,2))>= (num-1)) and (pow(root,2)<= (num+1))):
